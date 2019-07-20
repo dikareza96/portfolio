ebb43b380c0ce734270aee7541d77c46cd26a0f2
ahmadrifai561@gmail.com <ahmadrifai561@gmail.com>
2019-07-20 09:15:15 +0700

    fecth

---
+++ application/views/content/resume.php
@@ -0,0 +1,156 @@
+ <div id="lonon-main">
+            <!-- About Us -->
+            <div class="lonon-about">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12"> <span class="heading-meta style-1">Resume</span>
+                            <h2 class="lonon-heading animate-box" data-animate-effect="fadeInLeft">About Me</h2> </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-5"> <img src="images/resume.jpg" class="img-fluid mb-15 animate-box fadeInLeft animated" data-animate-effect="fadeInLeft" alt=""> </div>
+                        <div class="col-md-7 animate-box fadeInLeft animated" data-animate-effect="fadeInLeft"> <span class="heading-meta style-1">Resume</span>
+                            <h3 class="lonon-about-heading">I'am Web Designer</h3>
+                            <p>Quisque convallis lacinia est et volutpat. Ut interdum lectus velit, ac venenatis odio fringilla ut. Mauris tincidunt diam a nisi mollis rhoncus. Nam non ante consequatir, malesuada dui euismod, pharetra mi.</p>
+                            <p>Aenean convallis urna sapien, ut porta tortor maximus ac. Sed auctor, dui sed bib endum feugiat, felis lorem pulvinar mauris.</p>
+                            <p>Drana convallis lacinia est et volutpat. Ut interdum lectus velit, ac venenatis odio fringilla ut. Mauris tincidunt diam a nisi mollis rhoncus.</p>
+                            <div class="lonon-about-contact-wrap">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <p><b>Birthday:</b> 15 April 1980</p>
+                                        <p><b>Website:</b> www.lonon.com</p>
+                                        <p><b>Phone:</b> +1 650-123-0000</p>
+                                        <p><b>Skype:</b> lonon</p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p><b>Degree:</b> Master</p>
+                                        <p><b>Mail:</b> dev@lonon.com</p>
+                                        <p><b>Freelance:</b> Available</p>
+                                        <p><b>Linkedin:</b> lonon</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="divider1"></div>
+            <!-- Timeline Experience -->
+            <div class="lonon-timeline">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12"> <span class="heading-meta style-1">Resume</span>
+                            <h2 class="lonon-heading animate-box" data-animate-effect="fadeInLeft">Experience</h2> </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 animate-box" data-animate-effect="fadeInLeft">
+                            <div class="timeline lonon-timeline-second-style clearfix">
+                                <div class="lonon-timeline-item clearfix">
+                                    <div class="left-part">
+                                        <h5 class="item-period">2019</h5> <span class="item-company">Google</span> </div>
+                                    <div class="divider"></div>
+                                    <div class="right-part">
+                                        <h4 class="item-title">Senior Front-End Developer</h4>
+                                        <p>Education convallis lacinia est et volutpat. Ut interdum lectus velit, ac venenatis odio fringilla ut. Mauristincidunt diam a nisi mollis rhoncus. Nam non ante consequatir, malesuada euismomi.</p>
+                                    </div>
+                                </div>
+                                <div class="lonon-timeline-item clearfix">
+                                    <div class="left-part">
+                                        <h5 class="item-period">2014</h5> <span class="item-company">Apple</span> </div>
+                                    <div class="divider"></div>
+                                    <div class="right-part">
+                                        <h4 class="item-title">Senior UI/UX Designer</h4>
+                                        <p>Education convallis lacinia est et volutpat. Ut interdum lectus velit, ac venenatis odio fringilla ut. Mauristincidunt diam a nisi mollis rhoncus. Nam non ante consequatir, malesuada euismomi.</p>
+                                    </div>
+                                </div>
+                                <div class="lonon-timeline-item clearfix">
+                                    <div class="left-part">
+                                        <h5 class="item-period">2011</h5> <span class="item-company">Microsoft</span> </div>
+                                    <div class="divider"></div>
+                                    <div class="right-part">
+                                        <h4 class="item-title">Junior UI/UX Designer</h4>
+                                        <p>Education convallis lacinia est et volutpat. Ut interdum lectus velit, ac venenatis odio fringilla ut. Mauristincidunt diam a nisi mollis rhoncus. Nam non ante consequatir, malesuada euismomi.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="divider1"></div>
+            <!-- Timeline Education -->
+            <div class="lonon-timeline">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12"> <span class="heading-meta style-1">Resume</span>
+                            <h2 class="lonon-heading animate-box" data-animate-effect="fadeInLeft">Education</h2> </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 animate-box" data-animate-effect="fadeInLeft">
+                            <div class="timeline lonon-timeline-second-style clearfix">
+                                <div class="lonon-timeline-item clearfix">
+                                    <div class="left-part">
+                                        <h5 class="item-period">2008</h5> <span class="item-company">University</span> </div>
+                                    <div class="divider"></div>
+                                    <div class="right-part">
+                                        <h4 class="item-title">Master, Font-End Development</h4>
+                                        <p>Education convallis lacinia est et volutpat. Ut interdum lectus velit, ac venenatis odio fringilla ut. Mauristincidunt diam a nisi mollis rhoncus. Nam non ante consequatir, malesuada euismomi.</p>
+                                    </div>
+                                </div>
+                                <div class="lonon-timeline-item clearfix">
+                                    <div class="left-part">
+                                        <h5 class="item-period">2004</h5> <span class="item-company">University</span> </div>
+                                    <div class="divider"></div>
+                                    <div class="right-part">
+                                        <h4 class="item-title">University, Graphic Design</h4>
+                                        <p>Education convallis lacinia est et volutpat. Ut interdum lectus velit, ac venenatis odio fringilla ut. Mauristincidunt diam a nisi mollis rhoncus. Nam non ante consequatir, malesuada euismomi.</p>
+                                    </div>
+                                </div>
+                                <div class="lonon-timeline-item clearfix">
+                                    <div class="left-part">
+                                        <h5 class="item-period">1997</h5> <span class="item-company">Education</span> </div>
+                                    <div class="divider"></div>
+                                    <div class="right-part">
+                                        <h4 class="item-title">Highschool</h4>
+                                        <p>Education convallis lacinia est et volutpat. Ut interdum lectus velit, ac venenatis odio fringilla ut. Mauristincidunt diam a nisi mollis rhoncus. Nam non ante consequatir, malesuada euismomi.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="divider1"></div>
+            <!-- Skills -->
+            <div class="lonon-skills">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12"> <span class="heading-meta style-1">Abilities</span>
+                            <h2 class="lonon-heading animate-box" data-animate-effect="fadeInLeft">My Skills</h2> </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-5 animate-box" data-animate-effect="fadeInLeft">
+                            <p>Skills convallis lacinia est et volutpat. Ut interdum lecis velit, ac venenatis odio fringilla ut. Mauris tincidunt diame nisi mollis rhoncus. Nam non ante consequatir, malesuada dui euismod, pharetra mi.</p>
+                            <p>Sed auctor, dui sed bibendum feugiat, felis lorem pulvinar mauris vel porttitor risu nunc et risus. Aenean vehicula dictum magna non venenatis.</p>
+                        </div>
+                        <div class="col-md-7 animate-box" data-animate-effect="fadeInLeft">
+                            <p class="bar-title">JavaScript<span class="percent align-right">90%</span></p>
+                            <div class="bar">
+                                <div class="bar-fill bar-fill-developer start"></div>
+                            </div>
+                            <p class="bar-title">PHP<span class="percent align-right">80%</span></p>
+                            <div class="bar">
+                                <div class="bar-fill bar-fill-photoshop start"></div>
+                            </div>
+                            <p class="bar-title">HTML / CSS<span class="percent align-right">95%</span></p>
+                            <div class="bar">
+                                <div class="bar-fill bar-fill-webdesign start"></div>
+                            </div>
+                            <p class="bar-title">jQuery<span class="percent align-right">85%</span></p>
+                            <div class="bar">
+                                <div class="bar-fill bar-fill-socialmedia start"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          
---
+++ application/views/f_sidebar.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<!DOCTYPE HTML>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Lonon</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/favicon.png" />
+    <link href="https://fonts.googleapis.com/css?family=Rambla:400,700&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/css/animate.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/css/et-lineicons.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/css/themify-icons.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/css/bootstrap.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/css/style.css')?>">
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+        <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <div id="lonon-page"> <a href="#" class="js-lonon-nav-toggle lonon-nav-toggle"><i></i></a>
+        <!-- Sidebar Section -->
+        <aside id="lonon-aside">
+            <!-- Logo -->
+            <h1 id="lonon-logo">
+                <img src="images/profile.jpg" alt=""><br />
+                <a href="index.html">Lonon F. Smith<span>Web Designer</span></a>
+            </h1>
+            <!-- Menu -->
+            <nav id="lonon-main-menu">
+                <ul>
+                    <li class="lonon-active"><a href="index.html">Home</a></li>
+                    <li><a href="<?=site_url('data/');?>">Resume</a></li>
+                    <li><a href="portfolio.html">Portfolio</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav>
+            <!-- Sidebar Footer -->
+            <div class="lonon-footer">
+                <ul>
+                    <li><a href="#"><i class="ti-facebook font-14px gray-icon"></i></a></li>
+                    <li><a href="#"><i class="ti-twitter-alt font-14px gray-icon"></i></a></li>
+                    <li><a href="#"><i class="ti-instagram font-14px gray-icon"></i></a></li>
+                    <li><a href="#"><i class="ti-dribbble font-14px gray-icon"></i></a></li>
+                </ul>
+                <p><small>&copy; 2019 Lonon. by <a href="#">DuruThemes</a></small></p>
+            </div>
+        </aside>
+        
