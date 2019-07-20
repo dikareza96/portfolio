a1780a4c1e92cf596268eea5097e16633dba2882
Dika Reza <dikareza966@gmail.com>
2019-07-20 09:07:57 +0700

    update config, adding controller & views Signed-off-by: Dika Reza <dikareza966@gmail.com>

---
+++ application/controllers/backend/template.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class template extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		
+
+	}
+
+	public function index()
+	{
+		$this->load->view('backend/template/header');
+		$this->load->view('backend/template/sidebar');
+		$this->load->view('backend/home');
+		$this->load->view('backend/template/footer');
+	}
+
+}
