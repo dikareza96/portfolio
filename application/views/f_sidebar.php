625dec7ec3a743636ff681005ee9f1fd1aac4fec
ahmadrifai561@gmail.com <ahmadrifai561@gmail.com>
2019-07-20 10:08:03 +0700

    cange site_url ke base_url

--- application/views/f_sidebar.php
+++ application/views/f_sidebar.php
@@ -36,12 +36,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <!-- Menu -->
             <nav id="lonon-main-menu">
                 <ul>
-                    <li class="lonon-active"><a href="<?=site_url('data/index');?>">Home</a></li>
-                    <li><a href="<?=site_url('data/resume');?>">Resume</a></li>
-                    <li><a href="<?=site_url('data/portfolio');?>">Portfolio</a></li>
-                    <li><a href="<?=site_url('data/services');?>">Services</a></li>
-                    <li><a href="<?=site_url('data/blog');?>">Blog</a></li>
-                    <li><a href="<?=site_url('data/contact');?>">Contact</a></li>
+                    <li class="lonon-active"><a href="<?=base_url('data/index');?>">Home</a></li>
+                    <li><a href="<?=base_url('data/resume');?>">Resume</a></li>
+                    <li><a href="<?=base_url('data/portfolio');?>">Portfolio</a></li>
+                    <li><a href="<?=base_url('data/services');?>">Services</a></li>
+                    <li><a href="<?=base_url('data/blog');?>">Blog</a></li>
+                    <li><a href="<?=base_url('data/contact');?>">Contact</a></li>
                 </ul>
             </nav>
             <!-- Sidebar Footer -->
