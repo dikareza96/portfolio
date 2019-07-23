a0b29f94138bcebe90290784720fcb2ede58e822
Dika Reza <dikareza966@gmail.com>
2019-07-23 14:20:02 +0700

    adding project crud Signed-off-by: Dika Reza <dikareza966@gmail.com>

--- application/views/backend/template/sidebar.php
+++ application/views/backend/template/sidebar.php
@@ -35,6 +35,7 @@
                                  <li><a href="<?php echo base_url("backend/skill/index");?>"><i class="fa fa-angle-double-right"></i>Skill</a></li>
                                  <li><a href="<?php echo base_url("backend/testimonial/index");?>"><i class="fa fa-angle-double-right"></i>Testimonial</a></li>
                                   <li><a href="<?php echo base_url("backend/client/index");?>"><i class="fa fa-angle-double-right"></i>Client</a></li>
+                                   <li><a href="<?php echo base_url("backend/project/index");?>"><i class="fa fa-angle-double-right"></i>Project</a></li>
                                 
 
                             </ul>
