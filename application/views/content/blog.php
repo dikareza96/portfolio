f362cff675c22f2844654cbe9cec8bf0d9ae1528
Dika Reza <dikareza966@gmail.com>
2019-07-26 17:39:30 +0700

    fixing blog Signed-off-by: Dika Reza <dikareza966@gmail.com>

--- application/views/content/blog.php
+++ application/views/content/blog.php
@@ -7,32 +7,18 @@
                         <div class="col-md-12"> <span class="heading-meta style-1">Read</span>
                             <h2 class="lonon-heading animate-box" data-animate-effect="fadeInLeft">Blog</h2> </div>
                     </div>
-                    <?php foreach ($blog as $b): ?>
+                   
                     <div class="row">
-
-                        
-                            
-                       
-                        
+                         <?php foreach ($blog as $b): ?>
                         <div class="col-md-6 col-sm-6">
                             <div class="blog-entry animate-box" data-animate-effect="fadeInLeft">
-                                <a href="#" class="blog-img"><img src="<?=base_url()?>assets/uploads/<?=$b->img;?>" class="img-fluid" alt=""></a>
-                                <div class="desc"> <?php echo $b->title; ?></span>
-                                    <h3><a href="#"><?php echo $b->desc; ?></p>
+                                <a href="post.html" class="blog-img"><img src="<?=base_url()?>assets/uploads/<?=$b->img;?>" class="img-fluid" alt=""></a>
+                                <div class="desc"> <span>Sep 27, 2019 | Brand Identity</span>
+                                    <h3><a href="post.html"><?php echo $b->title; ?></a></h3>
+                                    <p><?php echo $b->desc; ?></p>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12 col-sm-12">
-                            <!-- Pagination -->
-                           <!--  <ul class="lonon-pagination-wrap align-center">
-                                <li><a href="blog.html#"><i class="ti-arrow-left"></i></a></li>
-                                <li><a href="blog.html#">1</a></li>
-                                <li><a href="blog.html#" class="active">2</a></li>
-                                <li><a href="blog.html#">3</a></li>
-                                <li><a href="blog.html#"><i class="ti-arrow-right"></i></a></li>
-                            </ul> -->
+                            
                         </div>
 
                          <?php endforeach ?>
